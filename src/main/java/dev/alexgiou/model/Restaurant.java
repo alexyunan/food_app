693cab6cf7f25d516bce8044b591ead15d294b45
Alexandros Giounan <alexandrosgiounan1@hotmail.com>
2024-02-24 20:09:28 +0200

    created Restaurant class.

---
+++ src/main/java/dev/alexgiou/model/Restaurant.java
@@ -0,0 +1,48 @@
+package dev.alexgiou.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Restaurant extends BaseModel {
+
+    @OneToOne
+    private User owner;
+
+    private String cuisineType;
+
+    @OneToOne
+    private Address address;
+
+    @Embedded
+    private ContactInformation contactInformation;
+
+    private String openingHours;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "restaurant", orphanRemoval = true)
+    private List<Order> orders = new ArrayList<>();
+
+    @ElementCollection
+    @Column(length = 1000)
+    private List<String> images;
+
+    private LocalDateTime registrationDate;
+
+    private boolean isOpen;
+
+    @JsonIgnore
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "restaurant", orphanRemoval = true)
+    private List<Food> foods = new ArrayList<>();
+}
