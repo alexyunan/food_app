48d5315a98649048cb7d149d81d22b896e32a2e8
Alexandros Giounan <alexandrosgiounan1@hotmail.com>
2024-02-24 20:12:14 +0200

    created ContactInformation class.

---
+++ src/main/java/dev/alexgiou/model/ContactInformation.java
@@ -0,0 +1,15 @@
+package dev.alexgiou.model;
+
+import lombok.Data;
+
+@Data
+public class ContactInformation {
+    
+    private String email;
+
+    private String mobile;
+
+    private String twitter;
+
+    private String instagram;
+}
