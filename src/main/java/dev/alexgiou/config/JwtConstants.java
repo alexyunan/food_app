d68b86f7d735148ec5ddab0d3a0443c51547cd02
Alexandros Giounan <alexandrosgiounan1@hotmail.com>
2024-02-25 12:19:56 +0200

    added expiration time.

--- src/main/java/dev/alexgiou/config/JwtConstants.java
+++ src/main/java/dev/alexgiou/config/JwtConstants.java
@@ -4,4 +4,7 @@ public class JwtConstants {
 
     public static final String SECRET_KEY = "asfjsabfasklfklsadjfbsaapasdjvoasdjvposjdapvosadvaspmvpasvaskvdposavmsdpoamvpsdaomvposda";
     public static final String JWT_HEADER = "Authorization";
+
+    public static final long TOKEN_EXPIRATION_TIME = 24 * 60 * 60 * 1000; // 24 hours in milliseconds
+
 }
