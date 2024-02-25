5884429c0eb426cf9bc953b38c07caae00092dd2
Alexandros Giounan <alexandrosgiounan1@hotmail.com>
2024-02-25 09:46:13 +0200

    Constants class.

---
+++ src/main/java/dev/alexgiou/config/JwtConstants.java
@@ -0,0 +1,7 @@
+package dev.alexgiou.config;
+
+public class JwtConstants {
+
+    public static final String SECRET_KEY = "asfjsabfasklfklsadjfbsaapasdjvoasdjvposjdapvosadvaspmvpasvaskvdposavmsdpoamvpsdaomvposda";
+    public static final String JWT_HEADER = "Authorization";
+}
