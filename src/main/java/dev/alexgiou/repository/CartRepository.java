0a3f60745191cc797dac0b244fc33f41216f58bf
Alexandros Giounan <alexandrosgiounan1@hotmail.com>
2024-02-25 12:20:15 +0200

    cart repository creation.

---
+++ src/main/java/dev/alexgiou/repository/CartRepository.java
@@ -0,0 +1,7 @@
+package dev.alexgiou.repository;
+
+import dev.alexgiou.model.cart.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CartRepository extends JpaRepository<Cart, Long> {
+}
