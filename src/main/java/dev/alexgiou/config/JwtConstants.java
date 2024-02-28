119a7bac7f7e67552b3ccf1d953687c1fdc1948f
Alexandros Giounan <alexandrosgiounan1@hotmail.com>
2024-02-28 17:59:15 +0200

    removed constant.

--- src/main/java/dev/alexgiou/config/JwtConstants.java
+++ src/main/java/dev/alexgiou/config/JwtConstants.java
@@ -5,6 +5,5 @@ public class JwtConstants {
     public static final String SECRET_KEY = "asfjsabfasklfklsadjfbsaapasdjvoasdjvposjdapvosadvaspmvpasvaskvdposavmsdpoamvpsdaomvposdgfhfghfdhfdgnfdgnfgna";
     public static final String JWT_HEADER = "Authorization";
 
-    public static final long TOKEN_EXPIRATION_TIME = 24 * 60 * 60 * 1000; // 24 hours in milliseconds
 
 }
