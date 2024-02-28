983bd756db6d38eaa36fa4a044c99d091d366b1d
Alexandros Giounan <alexandrosgiounan1@hotmail.com>
2024-02-28 10:41:48 +0200

    create restaurant class.

---
+++ src/main/java/dev/alexgiou/request/CreateRestaurantRequest.java
@@ -0,0 +1,21 @@
+package dev.alexgiou.request;
+
+import dev.alexgiou.model.Address;
+import dev.alexgiou.model.ContactInformation;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class CreateRestaurantRequest {
+
+    private Long id;
+    private String name;
+    private String description;
+    private String cuisineType;
+    private Address address;
+    private ContactInformation contactInformation;
+    private String openingHours;
+    private List<String> images;
+
+}
