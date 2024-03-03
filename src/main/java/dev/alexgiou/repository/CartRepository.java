120427b933f2f0f2821d0f20212fa17d07878204
Alexandros Giounan <alexandrosgiounan1@hotmail.com>
2024-03-03 15:54:05 +0200

    added find customer by id method.

--- src/main/java/dev/alexgiou/repository/CartRepository.java
+++ src/main/java/dev/alexgiou/repository/CartRepository.java
@@ -4,4 +4,5 @@ import dev.alexgiou.model.cart.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CartRepository extends JpaRepository<Cart, Long> {
+    Cart findByCustomerId(Long userId);
 }
